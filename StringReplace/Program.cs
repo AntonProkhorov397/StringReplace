8fadd9d4ad092b9d4e551b2990a466d3bef9c6ee
Opilane TTHK <opilane@tthk.ee>
2021-11-16 14:01:41 +0200

    Add project files.

---
+++ StringReplace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "hello";
+
+            hello = hello.Replace("h", "H");
+
+            Console.WriteLine(hello);
+            string panic = "D0n'7 p4n1c!";//Don't panic!
+
+            panic = panic.Replace("0", "o").Replace("7", "t").Replace("4", "a").Replace("1", "i")
+                
+            Console.WriteLine(panic);
+
+
+
+
+        }
+
+
+    }
+}
